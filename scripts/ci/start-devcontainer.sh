be239648b22dd8abae32782eab15ac52a9f6f3ab
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-07-28 12:19:37 +0000

    experiment/test/e2e-testing/17

--- scripts/ci/start-devcontainer.sh
+++ scripts/ci/start-devcontainer.sh
@@ -3,16 +3,20 @@
 WORKSPACE_PATH=$1
 CYPRESS_VERSION=$2
 
-for var in WORKSPACE_PATH CYPRESS_VERSION; do
+REQUIRED_VARS='WORKSPACE_PATH CYPRESS_VERSION'
+
+for var in $REQUIRED_VARS; do
   if [ -z "${!var}" ]; then 
     echo "Error: variable $var needs to be set"
     exit 1
   fi
 done
 
+for var in $REQUIRED_VARS; do
+  echo "Using ${var}: ${!var}"
+done
 
-
-CYPRESS_VERSION=CYPRESS_VERSION WORKSPACE_PATH=$WORKSPACE_PATH docker compose \
+CYPRESS_VERSION=$CYPRESS_VERSION WORKSPACE_PATH=$WORKSPACE_PATH docker compose \
   -f .docker/docker-compose.common.yml \
   -f .docker/docker-compose.e2e.ci.yml \
   up \
